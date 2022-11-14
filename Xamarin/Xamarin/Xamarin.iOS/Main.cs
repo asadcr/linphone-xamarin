9a04d83433c37a88070531e19972c749fa9ca484
Asad Ali <asadcr@live.com>
2022-11-14 21:11:39 +0500

    adding changes

--- Xamarin/Xamarin/Xamarin.iOS/Main.cs
+++ Xamarin/Xamarin/Xamarin.iOS/Main.cs
@@ -32,9 +32,7 @@ namespace TutoXamarin.iOS
 		// This is the main entry point of the application.
 		static void Main(string[] args)
 		{
-			// if you want to use a different Application Delegate class from "AppDelegate"
-			// you can specify it here.
-			UIApplication.Main(args, null, "AppDelegate");
+            UIApplication.Main(args, null, typeof(AppDelegate));
 		}
 	}
 }
